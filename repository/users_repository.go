7bbaa0c0afb2f56ec820b29b52f511fabb795144
abiyyu03 <abiyyu652@gmail.com>
2024-12-12 14:43:45 +0700

    update : add user repository

--- repository/users_repository.go
+++ repository/users_repository.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"errors"
-
 	"github.com/abiyyu03/siruta/config"
 	"github.com/abiyyu03/siruta/entity/model"
 	"github.com/google/uuid"
@@ -12,20 +10,17 @@ import (
 type UserRepository struct{}
 
 func (u *UserRepository) FetchLogin(username, password string) (*model.User, error) {
-	// if config.DB == nil {
-	// 	return nil, errors.New("database connection is not initialized")
-	// }
 	var user model.User
 
 	var query = config.DB.Where("username = ?", username).First(&user)
 	if err := query.Error; err != nil {
-		return nil, errors.New("Username or password not found")
+		// log.Panic(err)
+		return nil, err
 	}
 
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-	// var encryptedPassword = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
-		return nil, errors.New("Username or password not found")
+		return nil, err
 	}
 
 	return &user, nil
@@ -33,27 +28,25 @@ func (u *UserRepository) FetchLogin(username, password string) (*model.User, err
 }
 
 func (u *UserRepository) RegisterUser(user *model.User, roleId uint) (*model.User, error) {
-
-	id := uuid.New()
 	hashedPassword, err := bcrypt.GenerateFromPassword(
 		[]byte(user.Password),
-		10,
+		14,
 	)
 	if err != nil {
 		return nil, err
 	}
 
-	createdUser := model.User{
-		ID:        id.String(),
-		FirstName: user.FirstName,
-		LastName:  user.LastName,
-		RoleID:    roleId,
-		Email:     user.Email,
-		Username:  user.Username,
-		Password:  string(hashedPassword),
+	id := uuid.New()
+
+	createdUser := &model.User{
+		ID:       id.String(),
+		RoleID:   roleId,
+		Email:    user.Email,
+		Username: user.Username,
+		Password: string(hashedPassword),
 	}
 
 	config.DB.Create(&createdUser)
 
-	return &createdUser, nil
+	return createdUser, nil
 }
