b01993fa0dc4455d8878ab6f6babcc0b6c0086e2
abiyyu03 <abiyyu652@gmail.com>
2025-02-20 14:36:07 +0700

    refactor(rw-profile): implement clean arch in rw profile

--- usecase/rw_profile_usecase.go
+++ usecase/rw_profile_usecase.go
@@ -1,1 +1,31 @@
 package usecase
+
+import (
+	"github.com/abiyyu03/siruta/entity"
+	"github.com/abiyyu03/siruta/repository"
+	"github.com/gofiber/fiber/v2"
+)
+
+type RWProfileUsecase struct{}
+
+var rwProfileRepository = new(repository.RWProfileRepository)
+
+func (u *RWProfileUsecase) Fetch(ctx *fiber.Ctx) error {
+	rwProfiles, err := rwProfileRepository.Fetch()
+
+	if err != nil {
+		return entity.Error(ctx, fiber.StatusInternalServerError, "Server error")
+	}
+
+	return entity.Success(ctx, &rwProfiles, "Data fetched successfully")
+}
+
+func (u *RWProfileUsecase) FetchById(ctx *fiber.Ctx, id string) error {
+	rwProfile, err := rwProfileRepository.FetchById(id)
+
+	if err != nil {
+		return entity.Error(ctx, fiber.StatusNotFound, "RW Profile not found")
+	}
+
+	return entity.Success(ctx, &rwProfile, "Data fetched successfully")
+}
