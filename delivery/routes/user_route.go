a5e96d1c6c8e1fcd62780a6d0e4ae8323faa8859
abiyyu03 <abiyyu652@gmail.com>
2025-05-10 00:53:22 +0700

    refactor(route): user photo method and role access

--- delivery/routes/user_route.go
+++ delivery/routes/user_route.go
@@ -8,9 +8,12 @@ import (
 
 func RegisterUserRoutes(v1 fiber.Router, handler *http.UserHttp) {
 	adminOnly := middleware.JWTMiddleware([]int{1})
+	rw := middleware.JWTMiddleware([]int{2})
+	rt := middleware.JWTMiddleware([]int{3})
+	member := middleware.JWTMiddleware([]int{4})
 
 	v1.Get("/users", adminOnly, handler.GetData)
 	v1.Get("/users/:id", adminOnly, handler.GetDataById)
-	v1.Get("/users/photo/:id", adminOnly, handler.UpdateProfilePhoto)
+	v1.Put("/users/photo/:id", adminOnly, rw, rt, member, handler.UpdateProfilePhoto)
 	v1.Put("/users/revoke/:id", adminOnly, handler.RevokeUser)
 }
